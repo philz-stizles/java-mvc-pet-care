89971f6ab411607614c4888af0261194ea254264
philz-stizles <theophilusighalo@gmail.com>
2021-07-20 11:19:27 +0100

    first commit

---
+++ src/main/java/io/devdezyn/petcareapp/PetCareAppApplication.java
@@ -0,0 +1,13 @@
+package io.devdezyn.petcareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareAppApplication.class, args);
+	}
+
+}
